554da9262f6f11d986bdb2502146443e650c5b02
Minnael <minnaelcampelo@gmail.com>
2023-12-09 18:42:58 -0300

    add account create

---
+++ front/src/pages/Register/index.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom';
+import { LayoutComponents } from "../../components/LayoutComponents";
+import { useState } from 'react';
+import logoIMG from '../../assets/logoIMG.webp'; 
+
+export const Register = () => {
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+   const [name, setName] = useState("")
+
+   return (
+      <LayoutComponents>
+         <form className="login-form">
+            <span className="login-form-title">Criar Conta</span>
+
+            <span className="login-form-title">
+               <img src={logoIMG} alt="logoImg"/>
+            </span>
+
+            <div className="wrap-input">
+               <input 
+                  className={name !== "" ? 'has-val input' : 'input'}
+                  type="name"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+               />
+               <span className="focus-input" data-placeholder="Nome"></span>
+            </div>
+
+            <div className="wrap-input">
+               <input 
+                  className={email !== "" ? 'has-val input' : 'input'}
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+               />
+               <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+               <input 
+                  className={password !== "" ? 'has-val input' : 'input'}
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+               />
+               <span className="focus-input" data-placeholder="Password"></span>
+            </div>  
+
+            <div className="container-login-form-button">
+               <button className="login-form-button">CADASTRAR</button>
+            </div>
+
+            <div className="text-center">
+            <span className="txt1">Já possui uma conta?</span>
+               <Link className="txt2" to="/login">ACESSAR COM EMAIL E SENHA</Link>
+            </div>
+         </form>
+      </LayoutComponents>
+   );
+}
