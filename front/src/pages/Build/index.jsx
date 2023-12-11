2e7640516276207c01eaa2bac00ad1ad7e686307
Fabiany  Lopes <fabiany.lopes.060@ufrn.edu.br>
2023-12-11 20:51:56 -0300

    feat: build page

--- front/src/pages/Build/index.jsx
+++ front/src/pages/Build/index.jsx
@@ -1,3 +1,29 @@
+import "./styles.css";
+import ReactSelect from "react-select";
+import Logo from "../../assets/logo.png";
+
 export const Build = () => {
-  return <h2>Monte o Seu PC</h2>;
+  const countries = [
+    { value: "me", label: "Montenegro", image: "../../assets/logo.png" },
+    { value: "rs", label: "Serbia", image: "../../assets/logo.png" },
+  ];
+
+  return (
+    <div className="container">
+      <img src={Logo} alt="" />
+      {countries.map((element) => (
+        <ReactSelect
+          value={element.value}
+          options={countries}
+          formatOptionLabel={({ label }) => (
+            <div>
+              <img src={element.image} alt="" />
+              <p>{label}</p>
+              <p>Descrição...</p>
+            </div>
+          )}
+        />
+      ))}
+    </div>
+  );
 };
