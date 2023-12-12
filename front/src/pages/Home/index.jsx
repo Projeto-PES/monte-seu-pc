9f529a374c5ac1e93c2f7aecd8709ea5dae97c08
Fabiany  Lopes <fabiany.lopes.060@ufrn.edu.br>
2023-12-11 21:06:32 -0300

    refactor home

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -1,18 +1,9 @@
-import { Link } from "react-router-dom";
+import { Header } from "../../components/Header";
 
 export const Home = () => {
   return (
-    <div className="text-center">
-      <div className="container-login-form-button">
-        <Link className="txt2" to="/login">
-          <button className="login-form-button">Fazer Login</button>
-        </Link>
-      </div>
-      <div className="container-login-form-button">
-        <Link className="text-center" to="/build">
-          <button className="login-form-button">Montar PC</button>
-        </Link>
-      </div>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
