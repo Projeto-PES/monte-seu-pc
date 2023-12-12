c141aabf554c63b94055ceb7ae0027b02ed20abe
Marcos <41551000+Marcos7765@users.noreply.github.com>
2023-12-12 09:08:48 -0300

    add api sql drafts

---
+++ APIQueriesTest.sql
@@ -0,0 +1,65 @@
+-- 1. para cadastro
+-- 1.1 checar ruído
+SELECT usuario.ruido
+FROM usuario
+WHERE usuario.ruido = '<ruido>';
+-- 1.2 add usuario
+INSERT INTO usuario (email, nome, senha, ruido) VALUES
+('<email>', '<nome>', '<senha>', '<ruido>');
+
+-- 2. para login
+-- 2.1 validar usuario
+SELECT usuario.id
+FROM usuario
+WHERE usuario.email = '<email>' AND usuario.senha = '<senha>';
+-- 2.2 atualizar token
+UPDATE usuario
+SET usuario.token = '<token>'
+SET usuario.sessao = NOW()
+WHERE usuario.id = '<id>';
+-- 2.3 somente renovar sessão
+UPDATE usuario
+SET usuario.sessao = NOW()
+WHERE usuario.id = '<id>';
+
+-- 3. para pcs
+-- 3.1 listar pcs do usuário
+SELECT computadores.id, computadores.nome
+FROM computadores
+WHERE computadores.dono = '<id do usuario>';
+
+-- 4. para peças
+-- 4.1 obter propriedades das peças de um pc
+SELECT DISTINCT PECAS_PROP.prop
+FROM PECAS_PROP
+WHERE PECAS_PROP.peca IN (
+    SELECT COMP_PECAS.peca
+    FROM COMP_PECAS
+    WHERE COMP_PECAS.comp = '<id do pc>'
+);
+-- 4.1alt obter propriedades das peças de um pc usando JOIN
+SELECT DISTINCT pecas_prop.prop
+FROM pecas_prop
+INNER JOIN comp_pecas
+ON comp_pecas.peca = pecas_prop.peca
+WHERE comp_pecas.comp = '<id do pc>';
+-- 4.2 obter peças compatíveis com propriedades
+SELECT *
+FROM pecas
+WHERE pecas.id NOT IN (
+    SELECT pecas_prop.peca
+    FROM pecas_prop
+    INNER JOIN incompat_prop
+    ON incompat_prop.prop2 = pecas_prop.peca
+    WHERE incompat_prop.prop1 IN ('<prop1>','<prop2>','...')
+);
+-- 4.2.1 caso especial da fonte
+SELECT *
+FROM pecas
+WHERE pecas.id NOT IN (
+    SELECT pecas_prop.peca
+    FROM pecas_prop
+    INNER JOIN incompat_prop
+    ON incompat_prop.prop2 = pecas_prop.peca
+    WHERE incompat_prop.prop1 IN ('<prop1>','<prop2>','...')
+) AND pecas.pot > <pot total>;
