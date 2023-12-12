d3e462520e6d20318f4e7d82c86abef54e91563b
Minnael <minnaelcampelo@gmail.com>
2023-12-12 19:12:41 -0300

    ajustes no MontarPC

--- src/pages/Build/index.jsx
+++ src/pages/Build/index.jsx
@@ -1,82 +1,87 @@
 import "./styles.css";
 import ReactSelect from "react-select";
-import Logo from '../../assets/fundo.png'
 
 export const Build = () => {
   const memory = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: '../../assets/fundo.png'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: '../../assets/fundo.png'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: '../../assets/fundo.png'},
+    { name: "FURY - MEMÓRIA RAM 8GB", description: "ESSA MEMÓRIA POSSUI 8GB RAM", image: 'placa-mae.jpg'},
+    { name: "XPG - MEMÓRIA RAM 16GB", description: "ESSA MEMÓRIA POSSUI 16GB RAM", image: 'placa-mae.jpg'},
+    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "ESSA MEMÓRIA POSSUI 32GB RAM", image: 'placa-mae.jpg'},
   ];
 
   const motherboard = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: '../../assets/logoIMG.webp'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: '../../assets/logoIMG.webp'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: '../../assets/logoIMG.webp'},
+    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: 'placa-mae.jpg'},
+    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: 'placa-mae.jpg'},
+    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: 'placa-mae.jpg'},
   ];
 
   const powersupply = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: '../../assets/logoIMG.webp'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: '../../assets/logoIMG.webp'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: '../../assets/logoIMG.webp'},
+    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: 'placa-mae.jpg'},
+    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: 'placa-mae.jpg'},
+    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: 'placa-mae.jpg'},
   ];
 
   const processor = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: '../../assets/logoIMG.webp'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: '../../assets/logoIMG.webp'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: '../../assets/logoIMG.webp'},
+    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: 'placa-mae.jpg'},
+    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: 'placa-mae.jpg'},
+    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: 'placa-mae.jpg'},
   ];
 
 
   return (
     <div className="container">
-      <ReactSelect className="select-memory"
-        options={memory}
-        formatOptionLabel={({ name, description, image }) => (
-          <div>
-            <img src={image} alt={name} />
-            <p>{name}</p>
-            <p>{description}</p>
-          </div>
-        )}
-        placeholder = "MEMÓRIA RAM"
-      />
+      <div className="container-selects">
 
-      <ReactSelect className="select-motherboard"
-        options={motherboard}
-        formatOptionLabel={({ name, description, image }) => (
-          <div>
-            <img src={image} alt=""/>
-            <p>{name}</p>
-            <p>{description}</p>
-          </div>
-        )}
-        placeholder = "PLACA MÃE"
-      />
+        <h1>MONTE SEU PC!</h1>
 
-      <ReactSelect className="select-powersupply"
-        options={powersupply}
-        formatOptionLabel={({ name, description, image }) => (
-          <div>
-            <img src={image} alt=""/>
-            <p>{name}</p>
-            <p>{description}</p>
-          </div>
-        )}
-        placeholder = "FONTE"
-      />
+        <ReactSelect className="select-memory"
+          options={memory}
+          formatOptionLabel={({ name, description, image }) => (
+            <div className="select-control-memory">
+              <img src={image} alt=" " />
+              <p>{name}</p>
+              <p>{description}</p>
+            </div>
+          )}
+          placeholder = "MEMÓRIA RAM"
+        />
 
-      <ReactSelect className="select-processor"
-        options={processor}
-        formatOptionLabel={({ name, description, image }) => (
-          <div>
-            <img src={image} alt=""/>
-            <p>{name}</p>
-            <p>{description}</p>
-          </div>
-        )}
-        placeholder = "PROCESSADOR"
-      />
-  </div>
+        <ReactSelect className="select-motherboard"
+          options={motherboard}
+          formatOptionLabel={({ name, description, image }) => (
+            <div className="select-control-motherboard">
+              <img src={image} alt=" "/>
+              <p>{name}</p>
+              <p>{description}</p>
+            </div>
+          )}
+          placeholder = "PLACA MÃE"
+        />
+
+        <ReactSelect className="select-powersupply"
+          options={powersupply}
+          formatOptionLabel={({ name, description, image }) => (
+            <div className="select-control-powersupply">
+              <img src={image} alt=" "/>
+              <p>{name}</p>
+              <p>{description}</p>
+            </div>
+          )}
+          placeholder = "FONTE"
+        />
+
+        <ReactSelect className="select-processor"
+          options={processor}
+          formatOptionLabel={({ name, description, image }) => (
+            <div className="select-control-processor">
+              <img src={image} alt=" "/>
+              <p>{name}</p>
+              <p>{description}</p>
+            </div>
+          )}
+          placeholder = "PROCESSADOR"
+        />
+        
+      </div>
+    </div>
   );
 };
