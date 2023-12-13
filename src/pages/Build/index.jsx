a2244031e434a22e62c1cc3d1788fc2f42ec6746
Minnael <minnaelcampelo@gmail.com>
2023-12-13 15:05:24 -0300

    estilizando a tela de montagem de PC

--- src/pages/Build/index.jsx
+++ src/pages/Build/index.jsx
@@ -1,39 +1,39 @@
 import "./styles.css";
 import ReactSelect from "react-select";
+import logoIMG from '../../assets/logoIMG.webp'; 
 
 export const Build = () => {
   const memory = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "ESSA MEMÓRIA POSSUI 8GB RAM", image: 'placa-mae.jpg'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "ESSA MEMÓRIA POSSUI 16GB RAM", image: 'placa-mae.jpg'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "ESSA MEMÓRIA POSSUI 32GB RAM", image: 'placa-mae.jpg'},
+    { name: "FURY - MEMÓRIA RAM 8GB", description: "ESSA MEMÓRIA POSSUI 8GB RAM", image: 'memoria-ram.jpg'},
+    { name: "XPG - MEMÓRIA RAM 16GB", description: "ESSA MEMÓRIA POSSUI 16GB RAM", image: 'memoria-ram.jpg'},
+    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "ESSA MEMÓRIA POSSUI 32GB RAM", image: 'memoria-ram.jpg'},
   ];
 
   const motherboard = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: 'placa-mae.jpg'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: 'placa-mae.jpg'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: 'placa-mae.jpg'},
+    { name: "ASROCK B450M", description: "Steel Legend, AMD AM4, mATX, DDR4", image: 'placa-mae.jpg'},
+    { name: "GIGABYTE B450", description: "GAMING, AM4 2XDDR4(MÁX 64GB),4 SATA,1 M.2", image: 'placa-mae.jpg'},
+    { name: "GIGABYTE B550", description: "AORUS Elite, Chipset B550, AMD AM4, mATX, DDR4", image: 'placa-mae.jpg'},
   ];
 
   const powersupply = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: 'placa-mae.jpg'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: 'placa-mae.jpg'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: 'placa-mae.jpg'},
+    { name: "ATX 500W", description: "Bivolt Crusader Fortrek", image: 'fonte.webp'},
+    { name: "GIGABYTE 650W", description: "P650B 80 Plus Bronze PFC Ativo, GP-P650B", image: 'fonte.webp'},
+    { name: "ONE POWER 600W", description: "70% Eficiência - MP600W3-I, Preto, ATX , 220V/110V", image: 'fonte.webp'},
   ];
 
   const processor = [
-    { name: "FURY - MEMÓRIA RAM 8GB", description: "x", image: 'placa-mae.jpg'},
-    { name: "XPG - MEMÓRIA RAM 16GB", description: "y", image: 'placa-mae.jpg'},
-    { name: "ASGARD - MEMÓRIA RAM 32GB", description: "z", image: 'placa-mae.jpg'},
+    { name: "AMD RYZEN 5 5500", description: "‎Socket AM4, ‎65 watts, 3,6 GHz", image: 'processador.webp'},
+    { name: "CORE I9-10900KF", description: "3.4GHz (TURBO 4.6GHz) 32MB CACHE AM4 100-100000926WOF, Cerâmica cinza", image: 'processador.webp'},
+    { name: "AMD RYZEN 7 5700G", description: "3.7GHz (5.3GHz Max Turbo), Cache 20MB, LGA 1200", image: 'processador.webp'},
   ];
-
 
   return (
     <div className="container">
       <div className="container-selects">
 
-        <h1>MONTE SEU PC!</h1>
+        <img id="imgLogo" src={logoIMG}/>
 
-        <ReactSelect className="select-memory"
+        <ReactSelect className="select-control"
           options={memory}
           formatOptionLabel={({ name, description, image }) => (
             <div className="select-control-memory">
@@ -42,10 +42,9 @@ export const Build = () => {
               <p>{description}</p>
             </div>
           )}
-          placeholder = "MEMÓRIA RAM"
         />
 
-        <ReactSelect className="select-motherboard"
+        <ReactSelect className="select-control"
           options={motherboard}
           formatOptionLabel={({ name, description, image }) => (
             <div className="select-control-motherboard">
@@ -57,7 +56,7 @@ export const Build = () => {
           placeholder = "PLACA MÃE"
         />
 
-        <ReactSelect className="select-powersupply"
+        <ReactSelect className="select-control"
           options={powersupply}
           formatOptionLabel={({ name, description, image }) => (
             <div className="select-control-powersupply">
@@ -69,7 +68,7 @@ export const Build = () => {
           placeholder = "FONTE"
         />
 
-        <ReactSelect className="select-processor"
+        <ReactSelect className="select-control"
           options={processor}
           formatOptionLabel={({ name, description, image }) => (
             <div className="select-control-processor">
