7d51894910f768c610a61ea4845cde1b6bfc006c
Marcos <41551000+Marcos7765@users.noreply.github.com>
2023-12-14 19:58:57 -0300

    add home from frontProt

--- prot/app/page.js
+++ prot/app/page.js
@@ -1,53 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
+import "./styles.css";
+import Link from "next/link"
+const logoIMG = "/logoIMG.webp";
 
-
-const pags = [
-  {
-    nome: "Fazer login",
-    desc: "(não existiria tendo uma sessão implementada, você seria redirecionado direto à tela de login se não estivesse logado)",
-    dest: "/login"
-  },
-  {
-    nome: "Montar pc",
-    desc: "Monte seu pc evitando todas as incompatiblidades que conseguimos prever!",
-    dest: "/"
-  },
-  {
-    nome: "Gerenciar peças",
-    desc: "(Não elaborado por não pertencer ao protótipo e por não ter sido feita uma sessão, com sessão isso aqui só apareceria pra adm)",
-    dest: "/"
-  },
-  {
-    nome: "Deslogar",
-    desc: "(Não elaborado por não ter sido feita uma sessão)",
-    dest: "/"
-  },
-].sort((a, b) => a.nome > b.nome ? 1 : -1)
-
-const HorizontalCard = ({nome, desc}) => {
+export default function Home(){
   return (
-    <div className="bg-white rounded-lg shadow-lg" style={{height: '22vh', width: '44vh'}}>
-      <div className="p-4">
-        <h2 className="text-2xl font-bold mb-2 text-black">{nome}</h2>
-        <p className="text-gray-600">{desc}</p>
+    <div className="container">
+      <div>
+        <img id="imgLogo" src={logoIMG} />
+      </div>
+      <div className="flex flex-col justify-around" style={{height: "25vh"}}>
+        <p>O que deseja fazer?</p>
+        <Link href="/login">
+          <button className="home-button rounded-md">
+          Login
+        </button>
+        </Link>
+        <Link href="/montar">
+        <button className="home-button rounded-md">
+          Montar PC
+        </button>
+        </Link>
+        <Link href="/">
+        <button className="home-button rounded-md">
+          Gerenciar Peças
+        </button>
+        </Link>
+        <Link href="/">
+        <button className="home-button rounded-md">
+          Deslogar
+        </button>
+        </Link>
       </div>
     </div>
   );
 };
-
-
-const itens = pags.map(({nome, desc, dest}) => <li><Link href={dest}><HorizontalCard nome={nome} desc={desc}/></Link></li>)
-
-export default function Home() {
-  return (
-    <div className="mx-auto w-3/4 bg-gray-200 p-6 rounded-lg h-screen overflow-auto">
-      <div>
-        <h1 className="text-black text-4xl">Monte-seu-pc</h1>
-      </div>
-      <div className='flex justify-between flex-wrap'>
-      {itens}
-      </div>
-    </div>
-  )
-}
